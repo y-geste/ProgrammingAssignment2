22aaad15a85d107eda1ee1f53ba8377d01e700d6
y-geste <ygestegithub@gmail.com>
2015-08-23 17:14:28 -0400

    Added provided code

--- cachematrix.R
+++ cachematrix.R
@@ -25,3 +25,50 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
 }
+
+
+
+##
+##
+## makeVector creates a special "vector", which is really a list containing a function to
+##
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
+##
+## makevector is provided code
+## 
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
+## The following function calculates the mean of the special "vector" created with the above function. 
+##  it first checks to see if the mean has already been calculated. 
+##  iff so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data and sets the value of the mean in the cache 
+##  via the setmean function.
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
